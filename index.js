4cf10846c2f187213f8cec733485c31803659bf1
Sipu-982 <sipurana38@gmail.com>
2024-12-16 21:51:49 +0530

    Portfolio code updated

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+function toggleMenu() {
+  const menu = document.querySelector('.menu');
+  menu.classList.toggle('active');
+}
+
 document.addEventListener('scroll',()=>{
     const header=document.querySelector('header');
     if(window.scrollY>0){
